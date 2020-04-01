4f570cfe6ea3649c57ecc2af6c1c91ddbf0442b1
clicktripz <l87dg5f1!>
2020-04-01 13:51:34 -0700

    Added device factory

--- src/clicktripz/device/UserDeviceFactory.py
+++ src/clicktripz/device/UserDeviceFactory.py
@@ -6,21 +6,28 @@ from random import randint
 
 class UserDeviceFactory(object):
     def __init__(self, data_dir='data'):
-        # load user agents from the data file
-        with open(path.join(data_dir, 'browser_user_agents')) as f:
-            self.user_agents = [line.rstrip() for line in f]
-
-        # load US IPv4 from the data file
-        with open(path.join(data_dir, 'arin_us_ipv4')) as f:
-            self.us_ipv4 = [line.rstrip() for line in f]
-
-    def random_device(self):
-        pass
-
-    def random_ua(self):
-        return self.user_agents[randint(0, len(self.user_agents) - 1)]
+        self.user_agents = self.load_data(path.join(data_dir, 'browser_user_agents'))
+        self.us_ipv4 = self.load_data(path.join(data_dir, 'arin_us_ipv4'))
 
     @staticmethod
-    def random_ip():
+    def load_data(file):
+        with open(file) as f:
+            return [line.rstrip() for line in f]
+
+    def random_device(self):
+        return Device(ip=self.random_ipv4(), ua=self.random_ua())
+
+    def random_ua(self):
+        return self.random_peek(self.user_agents)
+
+    def random_us_ipv4(self):
+        return self.random_peek(self.us_ipv4)
+
+    @staticmethod
+    def random_peek(data):
+        return data[randint(0, len(data) - 1)]
+
+    @staticmethod
+    def random_ipv4():
         return '.'.join([str(randint(23, 216)) for x in range(4)])
 
