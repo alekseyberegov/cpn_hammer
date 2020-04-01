2c570fdefb3e98a58d4893117dd0928fc660e36e
clicktripz <l87dg5f1!>
2020-04-01 14:50:49 -0700

    Added IAB categories

---
+++ tests/testcases/clicktripz/iab/IABContentCategoryTestCase.py
@@ -0,0 +1,32 @@
+import unittest
+
+from clicktripz.iab.IABContentCategory import IABContentCategory
+
+
+class IABContentCategoryTestCase(unittest.TestCase):
+    def setUp(self):
+        self.category = IABContentCategory()
+
+    def tearDown(self):
+        del self.category
+
+    def test_get_desc_top_level(self):
+        desc = self.category.get_desc("IAB1")
+        self.assertListEqual(desc, ['Arts & Entertainment'])
+
+    def test_get_desc_second_level(self):
+        desc = self.category.get_desc("IAB1-1")
+        self.assertListEqual(desc, ['Books & Literature', 'Arts & Entertainment'])
+
+    def test_get_desc_unknown(self):
+        desc = self.category.get_desc("IAB40-1")
+        self.assertListEqual(desc, ['IAB40-1'])
+
+    def test_get_desc_malformed(self):
+        desc = self.category.get_desc("XUI")
+        self.assertListEqual(desc, ['XUI'])
+
+
+
+
+
