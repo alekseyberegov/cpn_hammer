1a336020eb87238d5a920e0fab1c174e21e341e2
clicktripz <l87dg5f1!>
2020-03-31 17:27:05 -0700

    Updated bid request

--- src/clicktripz/openrtb/request/BidRequest.py
+++ src/clicktripz/openrtb/request/BidRequest.py
@@ -1,5 +1,19 @@
+from clicktripz.serialize.Field import Field
+from clicktripz.serialize.String import String
+from clicktripz.serialize.Array import Array
+from clicktripz.serialize.Serializable import Serializable
 
 
 class BidRequest:
+    id = Field(String)
+
+    bcat = Field(Array(String))
+
+    imp = Field(Serializable)
+
+    site = Field(Serializable)
+
+    user = Field(Serializable)
+
     def __init__(self):
         pass
