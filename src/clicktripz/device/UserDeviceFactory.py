d13e875b5da772c42b9711e9a129919eff00440b
clicktripz <l87dg5f1!>
2020-04-01 13:57:35 -0700

    Added device factory

--- src/clicktripz/device/UserDeviceFactory.py
+++ src/clicktripz/device/UserDeviceFactory.py
@@ -6,8 +6,8 @@ from random import randint
 
 class UserDeviceFactory(object):
     def __init__(self, data_dir='data'):
-        self.user_agents = self.load_data(path.join(data_dir, 'browser_user_agents'))
-        self.us_ipv4 = self.load_data(path.join(data_dir, 'arin_us_ipv4'))
+        self.ua_list = self.load_data(path.join(data_dir, 'browser_user_agents'))
+        self.ip_list = self.load_data(path.join(data_dir, 'arin_us_ipv4'))
 
     @staticmethod
     def load_data(file):
@@ -15,13 +15,13 @@ class UserDeviceFactory(object):
             return [line.rstrip() for line in f]
 
     def random_device(self):
-        return Device(ip=self.random_ipv4(), ua=self.random_ua())
+        return Device(ip=self.random_us_ipv4(), ua=self.random_ua())
 
     def random_ua(self):
-        return self.random_peek(self.user_agents)
+        return self.random_peek(self.ua_list)
 
     def random_us_ipv4(self):
-        return self.random_peek(self.us_ipv4)
+        return self.random_peek(self.ip_list)
 
     @staticmethod
     def random_peek(data):
--- tests/testcases/clicktripz/device/UserDeviceFactoryTestCase.py
+++ tests/testcases/clicktripz/device/UserDeviceFactoryTestCase.py
@@ -13,5 +13,15 @@ class UserDeviceFactoryTestCase(unittest.TestCase):
     def tearDown(self):
         del self.factory
 
-    def test_generate_ua(self):
+    def test_random_ua(self):
         self.assertIsNotNone(self.factory.random_ua())
+
+    def test_random_us_ipv4(self):
+        self.assertIsNotNone(self.factory.random_us_ipv4())
+
+    def test_random_device(self):
+        device = self.factory.random_device()
+        self.assertIsNotNone(device)
+        self.assertIsNotNone(device.ip)
+        self.assertIsNotNone(device.ua)
+
