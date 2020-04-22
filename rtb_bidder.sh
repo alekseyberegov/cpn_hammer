3810b05bcd454886cc478a3f315821be296aba55
clicktripz <l87dg5f1!>
2020-04-22 14:18:45 -0700

    update the doc

--- rtb_bidder.sh
+++ rtb_bidder.sh
@@ -2,7 +2,7 @@
 
 DIR_NAME=$( dirname "${BASH_SOURCE[0]}" )
 
-SITE_PACKAGES=`cat ~/.site-packages.env`
+SITE_PACKAGES=$(cat ~/.site-packages.env)
 export PYTHONPATH=$SITE_PACKAGES:$PYTHONPATH
 
 python3 cli/Server.py run $1 $2 $3
