2f06dee510a4f157c5759a8f46de47f9aaaa212e
clicktripz <l87dg5f1!>
2020-05-01 09:30:29 -0700

    Better error reporting

--- src/clicktripz/http/ResponseFactory.py
+++ src/clicktripz/http/ResponseFactory.py
@@ -4,10 +4,9 @@ from clicktripz.http.utils import make_url
 class ResponseFactory(object):
     def __init__(self):
         self.adm_url = make_url('https://staycation.tiki.com'
-                                , 'cant-travel-turn-your-quarantine-into-a-staycation'
                                 , utm_source=2949
                                 , utm_campaign='staycation'
-                                , utm_medium='fslb-20'
+                                , utm_medium='LBemail'
                                 , bid='${AUCTION_PRICE}')
 
     def get_adm_url(self):
