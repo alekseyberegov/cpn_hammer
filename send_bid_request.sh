90482b34208935e21052e7696fdc1577eb7302cb
clicktripz <l87dg5f1!>
2020-05-14 21:51:08 -0700

    updated script

--- send_bid_request.sh
+++ send_bid_request.sh
@@ -8,21 +8,25 @@ key="$1"
 case $key in
     -h|--help)
     echo ""
-    echo "Usage: $0 [-c|--count <request count>] [-e|--endpoint <endpoint>] [--silent]"
+    echo "Usage: $0 [-c|--count <request count>] [-e|--endpoint <endpoint>] [-d|--debug] [--silent]"
     exit 1
     ;;
     -c|--count)
-    COUNT="$2"
+    count="$2"
     shift # past argument
     shift # past value
     ;;
     -e|--endpoint)
-    ENDPOINT="$2"
+    endpoint="$2"
     shift # past argument
     shift # past value
     ;;
+    -d|--debug)
+    debug="TRUE"
+    shift # past argument
+    ;;
     --silent)
-    SILENT=" -silent --output /dev/null "
+    silent=" -silent --output /dev/null "
     shift # past argument
     ;;
     *)    # unknown option
@@ -33,28 +37,42 @@ esac
 done
 set -- "${POSITIONAL[@]}" # restore positional parameters
 
-COUNT=${COUNT:-1}
-ENDPOINT=${ENDPOINT:-"http://127.0.0.1:9000"}
+count=${count:-1}
+endpoint=${endpoint:-"http://127.0.0.1:9000"}
 
-echo "COUNT         = ${COUNT}"
-echo "ENDPOINT      = ${ENDPOINT}"
-echo "SILENT        = ${SILENT}"
-
+#hdr_host_name=$(echo $endpoint | awk -F[/:] '{print $4}')
+proto="$(echo ${endpoint} | grep :// | sed -e's,^\(.*://\).*,\1,g')"
+url="$(echo ${endpoint/$proto/})"
+hdr_host_name="$(echo ${url} | cut -d/ -f1)"
+hdr_cont_type="Content-Type: application/json"
+hdr_open_rtb="x-openrtb-version: 2.5"
 
 dev_ua="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36"
 dev_ip="108.185.179.0"
 pub_id="2949"
-pub_ct="[\"sport\", \"coats\", \"spring\"]"
+pub_cat="[\"sport\", \"coats\", \"spring\"]"
 
-for i in `eval echo {1..$COUNT}`
+if [[ "$debug" = "TRUE" ]]
+then
+  echo ""
+  echo "silent   = ${silent}"
+  echo "count    = ${count}"
+  echo "endpoint = ${endpoint}"
+  echo "content  = ${hdr_cont_type}"
+  echo "host     = ${hdr_host_name}"
+  echo "rtb      = ${hdr_open_rtb}"
+  echo ""
+fi
+
+for i in `eval echo {1..$count}`
 do
   req_id=$(uuidgen)
   usr_id=$(uuidgen)
 
-  curl $SILENT --location --request POST ${ENDPOINT} \
-  --header 'Content-Type: application/json' \
-  --header 'Host: engine.4dsply.com' \
-  --header 'x-openrtb-version: 2.5' \
+  curl $silent --location --request POST ${endpoint} \
+  --header "${hdr_cont_type}" \
+  --header "${hdr_host_name}" \
+  --header "${hdr_open_rtb}" \
   --data-raw "{
       \"id\" : \"${req_id}\",
       \"bcat\" : [],
@@ -66,7 +84,7 @@ do
       ],
       \"site\": {
           \"id\": \"${pub_id}\",
-          \"cat\" : ${pub_ct}
+          \"cat\" : ${pub_cat}
       },
       \"device\": {
           \"ua\": \"${dev_ua}\",
@@ -76,7 +94,7 @@ do
           \"id\": \"${usr_id}\"
       }
   }"
-  if [[ "$SILENT" = "" ]]
+  if [[ "$silent" = "" ]]
   then
     echo ""
   fi
