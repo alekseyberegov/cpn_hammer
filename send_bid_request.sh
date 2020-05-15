ae15e81455ba6a6b159b4e4261989ecc4877ed91
clicktripz <l87dg5f1!>
2020-05-14 19:33:31 -0700

    updated script

--- send_bid_request.sh
+++ send_bid_request.sh
@@ -40,33 +40,42 @@ echo "COUNT         = ${COUNT}"
 echo "ENDPOINT      = ${ENDPOINT}"
 echo "SILENT        = ${SILENT}"
 
+
+dev_ua="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36"
+dev_ip="108.185.179.0"
+pub_id="2949"
+pub_ct="[\"sport\", \"coats\", \"spring\"]"
+
 for i in `eval echo {1..$COUNT}`
 do
+  req_id=$(uuidgen)
+  usr_id=$(uuidgen)
+
   curl $SILENT --location --request POST ${ENDPOINT} \
   --header 'Content-Type: application/json' \
   --header 'Host: engine.4dsply.com' \
   --header 'x-openrtb-version: 2.5' \
-  --data-raw '{
-      "id" : "fffd894f-c33c-4c08-b826-4448fcaecc85",
-      "bcat" : [ "CTX-1", "CTX-3", "CTX-4" ],
-      "imp": [
+  --data-raw "{
+      \"id\" : \"${req_id}\",
+      \"bcat\" : [],
+      \"imp\": [
           {
-          "id": "aaad894f-c33c-4c08-b826-4448fcaecc899",
-           "instl": 1
+          \"id\": \"${req_id}\",
+          \"instl\": 1
           }
       ],
-      "site": {
-          "id": "19283",
-          "cat" : [ "IAB12", "IAB12-5", "coats", "spring" ]
+      \"site\": {
+          \"id\": \"${pub_id}\",
+          \"cat\" : ${pub_ct}
       },
-      "device": {
-          "ua": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36",
-          "ip": "108.185.179.0"
+      \"device\": {
+          \"ua\": \"${dev_ua}\",
+          \"ip\": \"${dev_ip}\"
       },
-      "user": {
-          "id": "08057c4b-4cb3-4e65-8a9b-b6db49d921a7"
+      \"user\": {
+          \"id\": \"${usr_id}\"
       }
-  }'
+  }"
   if [[ "$SILENT" = "" ]]
   then
     echo ""
